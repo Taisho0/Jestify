b4328dde2cd83e580227740045da73790f8955a6
Taisho <merx230@gmail.com>
2024-12-30 20:17:57 +0800

    Update Main.js

--- js/Main.js
+++ js/Main.js
@@ -6,3 +6,38 @@ window.addEventListener("blur", () => {
 window.addEventListener("focus", () => {
     document.title = doctitle;
 });
+
+
+//config Tailwind
+
+tailwind.config = {
+  theme: {
+    backgroundColor: {
+      'neonGreen': 'rgb(42, 250, 45)',
+      'red': 'rgb(250,0,0)',
+      'blue': 'rgb(0,0,250)',
+    },
+    boxShadow: {
+      'navglow': '0 0 10px 2px rgb(42, 250, 45)'
+    },
+    dropShadow: {
+      'navtextglow': '0 0 6px rgb(42, 250, 45)',
+      'ceo': '0 0 6px rgb(255, 247, 0)'
+    },
+    backdropBlur: {
+      8: '8px',
+    },
+    blur: {
+      xs: '2px',
+    },
+      
+    //This is for foreign source
+    extend: {
+      backgroundImage: {
+        'main': "url('/img/LoginPage.jpeg')",
+        'logo': "url('/img/JestifyLogo0.png')",
+        'ceo': "url('/img/CEO.png')"
+      },
+    }
+  }
+}
