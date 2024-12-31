3f42959bbdb97968aa863252a7d39d439456bdee
Taisho <merx230@gmail.com>
2024-12-31 11:52:58 +0800

    Delete public/js/Main.js

--- public/js/Main.js
+++
@@ -1,8 +0,0 @@
-//tab interactive
-let doctitle = document.title;
-window.addEventListener("blur", () => {
-    document.title = "Comeback Senpai🥺";
-});
-window.addEventListener("focus", () => {
-    document.title = doctitle;
-});
